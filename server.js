37d9d36901fb9f0ca479adfaf7013b903c4c5c24
mauv2sky <mauv2sky@gmail.com>
2021-05-20 19:37:20 +0900

    add movies mutations, queries

--- server.js
+++ server.js
@@ -1,44 +1,10 @@
-import {ApolloServer, gql} from "apollo-server"
-
-const typeDefs = gql`
-    type Movie {
-        title: String,
-        year: Int
-    }    
-
-    type Query {
-        movies: [Movie],
-        movie: Movie
-    }
-
-    type Mutation {
-        createMovie(title:String!): Boolean,
-        deleteMovie(title:String!): Boolean
-    }
-`
-
-const resolvers = {
-    Query: {
-        movies: () => [],
-        movie: () => ({title: "Hello", year: 2021})
-    },
-    Mutation: {
-        createMovie: (_, { title }) => {
-            console.log(title);
-            return true;
-        },
-        deleteMovie: (_, { title }) => {
-            console.log(title);
-            return true;
-        }
-    }
-}
+import { ApolloServer } from "apollo-server";
+import { typeDefs, resolvers} from "./schema";
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
-})
-
+  typeDefs,
+  resolvers,
+});
 server
-.listen()
-.then(() => console.log("Server is running on http://localhost:4000/"))
+  .listen()
+  .then(() => console.log("Server is running on http://localhost:4000/"));
