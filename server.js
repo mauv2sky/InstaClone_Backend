36d3f2eebfad953ce9e8a4a5af2f8214664bca6d
mauv2sky <mauv2sky@gmail.com>
2021-05-21 15:51:56 +0900

    merge typeDefs, resolvers to schema.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
 import { ApolloServer } from "apollo-server";
-import { typeDefs, resolvers} from "./schema";
+import schema from "./schema";
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema
 });
+
+const PORT = process.env.PORT
+
 server
-  .listen()
-  .then(() => console.log("Server is running on http://localhost:4000/"));
+  .listen(PORT)
+  .then(() => console.log(`Server is running on http://localhost:${PORT}/ ✅`));
