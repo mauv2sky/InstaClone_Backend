7c18b12bfbdea798aa97d675a27e472184b29d36
mauv2sky <mauv2sky@gmail.com>
2021-05-21 15:59:42 +0900

    create a parent folder of movies

--- schema.js
+++ schema.js
@@ -1,7 +1,7 @@
 import { loadFilesSync,mergeResolvers,mergeTypeDefs,makeExecutableSchema } from "graphql-tools";
 
-const loadedTypeDefs = loadFilesSync(`${__dirname}/**/*.typeDefs.js`)
-const loadedResolvers = loadFilesSync(`${__dirname}/**/*.{queries,mutations}.js`)
+const loadedTypeDefs = loadFilesSync(`${__dirname}/gql/**/*.typeDefs.js`)
+const loadedResolvers = loadFilesSync(`${__dirname}/gql/**/*.{queries,mutations}.js`)
 
 const typeDefs = mergeTypeDefs(loadedTypeDefs)
 const resolvers = mergeResolvers(loadedResolvers)
