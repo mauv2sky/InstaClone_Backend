0b58920cf6ed446069a622cd8fc1ad7abfc86e08
mauv2sky <mauv2sky@gmail.com>
2021-05-26 13:04:20 +0900

    add functions for login, editProfile

--- gql/User/user.mutations.js
+++ gql/User/user.mutations.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 import client from "../../client";
 
 
@@ -39,6 +40,33 @@ export default {
             } catch(e) {
                 return e;
             }
+        },
+        login: async (_, {userName, password}) => {
+            // find user with args.userName
+            const user = await client.user.findFirst({ where: {userName} });
+            if(!user){
+                return {
+                    ok: false,
+                    error: "User not found."
+                }
+            }
+            // check password with args.password
+            const passwordOk = await bcrypt.compare(password, user.password);
+            if(!passwordOk){
+                return {
+                    ok: false,
+                    error: "Incorrect password."
+                }
+            }
+            // send token
+            const token = jwt.sign({id: user.id}, process.env.SECRET_KEY);
+            return {
+                ok: true,
+                token
+            }
+        },
+        editProfile: async (_, {firstName, lastName, email, userName, password}) => {
+            
         }
     }
 }
--- gql/User/user.typeDefs.js
+++ gql/User/user.typeDefs.js
@@ -10,6 +10,15 @@ export default gql`
         createdAt: String!
         updatedAt: String!
     },
+    type loginResult {
+        ok: Boolean!
+        error: String
+        token: String
+    },
+    type editProfileResult {
+        ok: Boolean!
+        error: String
+    }
     type Mutation {
         createAccount(
             firstName: String!
@@ -17,7 +26,18 @@ export default gql`
             userName: String!
             email: String!
             password: String!
-        ): User
+        ): User,
+        login(
+            userName: String!
+            password: String!
+        ): loginResult!,
+        editProfile(
+            firstName: String
+            lastName: String
+            userName: String
+            email: String
+            password: String
+        ): editProfileResult!
     },
     type Query {
         seeProfile(userName: String!): User
