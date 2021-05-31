d4adb80babad1e4102c30ecc81e9135bb753ce35
mauv2sky <mauv2sky@gmail.com>
2021-05-31 11:32:18 +0900

    made createAccount folder

---
+++ gql/User/createAccount/createAccount.resolvers.js
@@ -0,0 +1,40 @@
+import client from "../../../client"
+import bcrypt from "bcrypt"
+
+export default {
+    createAccount: async (_, {
+        firstName, 
+        lastName,
+        userName,
+        email,
+        password }
+    ) => {
+        try{
+            // check if username or email are already on DB
+            const existingUser = await client.user.findFirst({
+                where: {
+                    OR: [
+                        { userName },
+                        { email }
+                    ]
+                }
+            });
+
+            if(existingUser){
+                return new Error("This username/password is already token.")
+            }
+            const uglyPassword = await bcrypt.hash(password, 10);
+            return client.user.create({
+                data: {
+                    firstName,
+                    lastName,
+                    email,
+                    userName,
+                    password: uglyPassword
+                }
+            });
+        } catch(e) {
+            return e;
+        }
+    }
+}
