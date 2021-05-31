e0015bcfd6accd72d80df9e715299d4e99a5c331
mauv2sky <mauv2sky@gmail.com>
2021-05-31 11:33:19 +0900

    made editProfile folder

---
+++ gql/User/editProfile/editProfile.resolvers.js
@@ -0,0 +1,8 @@
+import client from "../../../client";
+
+export default {
+    Mutation : {
+        editProfile: async (_, {firstName, lastName, email, userName, password}) => {
+        }
+    }
+}
