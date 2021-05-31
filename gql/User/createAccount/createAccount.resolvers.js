db5e40993b3ebd3e7f7a2cc1931ec2099942c29e
mauv2sky <mauv2sky@gmail.com>
2021-05-31 16:39:08 +0900

    fix createAccount code

--- gql/User/createAccount/createAccount.resolvers.js
+++ gql/User/createAccount/createAccount.resolvers.js
@@ -2,39 +2,41 @@ import client from "../../../client"
 import bcrypt from "bcrypt"
 
 export default {
-    createAccount: async (_, {
-        firstName, 
-        lastName,
-        userName,
-        email,
-        password }
-    ) => {
-        try{
-            // check if username or email are already on DB
-            const existingUser = await client.user.findFirst({
-                where: {
-                    OR: [
-                        { userName },
-                        { email }
-                    ]
+    Mutation : {
+        createAccount: async (_, {
+            firstName, 
+            lastName,
+            userName,
+            email,
+            password }
+        ) => {
+            try{
+                // check if username or email are already on DB
+                const existingUser = await client.user.findFirst({
+                    where: {
+                        OR: [
+                            { userName },
+                            { email }
+                        ]
+                    }
+                });
+    
+                if(existingUser){
+                    return new Error("This username/password is already token.")
                 }
-            });
-
-            if(existingUser){
-                return new Error("This username/password is already token.")
+                const uglyPassword = await bcrypt.hash(password, 10);
+                return client.user.create({
+                    data: {
+                        firstName,
+                        lastName,
+                        email,
+                        userName,
+                        password: uglyPassword
+                    }
+                });
+            } catch(e) {
+                return e;
             }
-            const uglyPassword = await bcrypt.hash(password, 10);
-            return client.user.create({
-                data: {
-                    firstName,
-                    lastName,
-                    email,
-                    userName,
-                    password: uglyPassword
-                }
-            });
-        } catch(e) {
-            return e;
         }
     }
 }
