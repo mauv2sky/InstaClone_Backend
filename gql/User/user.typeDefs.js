c703df2413a46188a305eb57373207f57878d524
mauv2sky <mauv2sky@gmail.com>
2021-05-31 11:34:23 +0900

    move a codes to other folders

--- gql/User/user.typeDefs.js
+++ gql/User/user.typeDefs.js
@@ -9,37 +9,5 @@ export default gql`
         email: String!
         createdAt: String!
         updatedAt: String!
-    },
-    type loginResult {
-        ok: Boolean!
-        error: String
-        token: String
-    },
-    type editProfileResult {
-        ok: Boolean!
-        error: String
-    }
-    type Mutation {
-        createAccount(
-            firstName: String!
-            lastName: String
-            userName: String!
-            email: String!
-            password: String!
-        ): User,
-        login(
-            userName: String!
-            password: String!
-        ): loginResult!,
-        editProfile(
-            firstName: String
-            lastName: String
-            userName: String
-            email: String
-            password: String
-        ): editProfileResult!
-    },
-    type Query {
-        seeProfile(userName: String!): User
     }
 `
