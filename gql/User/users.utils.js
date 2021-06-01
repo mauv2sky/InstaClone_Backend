c80af6b5a460a9bb503d118ccbc54895b33a9649
mauv2sky <mauv2sky@gmail.com>
2021-06-01 22:52:42 +0900

    add getUser code

---
+++ gql/User/users.utils.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken"
+import client from "../../client";
+
+export const getUser = async (token) => {
+    try{
+        if(!token){
+            return null;
+        }
+        const { id } = jwt.verify(token, process.env.SECRET_KEY);
+        const loggedInUser = await client.user.findUnique({where: {id}});
+        if(loggedInUser){
+            return loggedInUser;
+        } else {
+            return null;
+        }
+    } catch {
+        return null;
+    }
+}
