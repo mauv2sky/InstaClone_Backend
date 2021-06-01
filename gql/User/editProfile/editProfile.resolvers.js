abf8e47c08861a2c18211916a11d7055681dd22a
mauv2sky <mauv2sky@gmail.com>
2021-06-01 22:53:14 +0900

    update editProfile code

--- gql/User/editProfile/editProfile.resolvers.js
+++ gql/User/editProfile/editProfile.resolvers.js
@@ -1,8 +1,47 @@
 import client from "../../../client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export default {
     Mutation : {
-        editProfile: async (_, {firstName, lastName, email, userName, password}) => {
+        editProfile: async (
+            _,
+            {
+                firstName, 
+                lastName, 
+                email, 
+                userName, 
+                password: newPassword
+            },
+            { loggedInUser }
+        ) => {
+            let uglyPassword  = null;
+            if(newPassword){
+                uglyPassword = await bcrypt.hash(newPassword, 10);
+            }
+
+            const updatedUser = await client.user.update({
+                where: {
+                    id: loggedInUser.id
+                },
+                data: {
+                    firstName, 
+                    lastName, 
+                    email, 
+                    userName, 
+                    ...(uglyPassword && {password: uglyPassword})
+                }
+            });
+            if(updatedUser.id){
+                return {
+                    ok: true
+                }
+            } else {
+                return {
+                    ok: false,
+                    error: "Could not update profile."
+                }
+            }
         }
     }
 }
