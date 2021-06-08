f07a5cd558ee43f20d4ee190ef9550869b9563f5
mauv2sky <mauv2sky@gmail.com>
2021-06-08 21:29:51 +0900

    add seeFollowings resolver, typeDef

---
+++ gql/User/seeFollowings/seeFollowings.resolvers.js
@@ -0,0 +1,30 @@
+import client from "../../../client"
+
+export default {
+    Query: {
+        seeFollowings: async (_, {userName, lastId}) => {
+            const ok = await client.user.findUnique({
+                where: {userName},
+                select: {id: true}
+            });
+            if(!ok){
+                return {
+                    ok: false,
+                    error: "User not found."
+                }
+            }
+            // use cursor based pagination
+            const followings = await client.user
+                .findUnique({ where: {userName} })
+                .following({
+                    take: 5,
+                    skip: lastId ? 1: 0,
+                    ...(lastId && {cursor: {id: lastId}})
+                });
+            return {
+                ok: true,
+                followings
+            }
+        }
+    }
+}
