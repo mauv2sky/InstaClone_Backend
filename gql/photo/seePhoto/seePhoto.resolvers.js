53adeb4bdc6d88beec14ebf76bd703ba96e4fd27
mauv2sky <mauv2sky@gmail.com>
2021-06-10 17:05:10 +0900

    add seePhoto resolver, typeDef

---
+++ gql/photo/seePhoto/seePhoto.resolvers.js
@@ -0,0 +1,13 @@
+import client from "../../../client"
+
+export default {
+    Query: {
+        seePhoto: (_, { id }) => {
+            return client.photo.findUnique({
+                where: {
+                    id
+                }
+            })
+        }
+    }
+}
