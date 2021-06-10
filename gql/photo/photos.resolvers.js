4f839de9096168d11460e810d2c92596861b15f3
mauv2sky <mauv2sky@gmail.com>
2021-06-10 17:06:38 +0900

    add resolver for photo's computed fleids

---
+++ gql/photo/photos.resolvers.js
@@ -0,0 +1,22 @@
+import client from "../../client"
+
+export default {
+    Photo: {
+        user: ({ userId }) => {
+            return client.user.findUnique({
+                where: {
+                    id: userId
+                }
+            })
+        },
+        hashtags: ({ id }) => {
+            return client.hashtag.findMany({
+                where: {
+                    photos: {
+                        some: { id }
+                    }
+                }
+            })
+        }
+    }
+}
