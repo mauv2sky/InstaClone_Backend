e4b7a3753d61aacee8831e5a9155ba87aa1ede1b
mauv2sky <mauv2sky@gmail.com>
2021-06-14 14:59:20 +0900

    add resolvers for photo, hashtag

--- gql/photo/photos.resolvers.js
+++ gql/photo/photos.resolvers.js
@@ -17,6 +17,32 @@ export default {
                     }
                 }
             })
+        },
+        likes: ({ id }) => {
+            return client.like.count({
+                where: {
+                    photoId: id
+                }
+            })
         }
+    },
+    Hashtag: {
+        photos: ({ id }, { page }) => {
+            return client.hashtag.findUnique({
+                where: {
+                    id
+                }
+            }).photos();
+        },
+        totalPhotos: ({ id }) =>
+            client.photo.count({
+                where: {
+                    hashtags: {
+                        some: {
+                            id
+                        }
+                    }
+                }
+            })
     }
 }
--- gql/photo/photos.typeDefs.js
+++ gql/photo/photos.typeDefs.js
@@ -6,6 +6,7 @@ export default gql`
         user: User!
         file: String!
         caption: String
+        likes: Int!
         hashtags: [Hashtag]
         createdAt: String!
         updatedAt: String!
@@ -13,7 +14,15 @@ export default gql`
     type Hashtag {
         id: Int!
         hashtag: String!
-        photo: [Photo]
+        photos(page: Int!): [Photo]
+        totalPhotos: Int!
+        createdAt: String!
+        updatedAt: String!
+    },
+    type Like {
+        id: Int!
+        user: User!
+        photo: Photo!
         createdAt: String!
         updatedAt: String!
     }
