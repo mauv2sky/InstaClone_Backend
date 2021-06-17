0ecad21fc8c26cd93bfc63285c3e882c5f1bff8d
mauv2sky <mauv2sky@gmail.com>
2021-06-17 16:06:47 +0900

    add graphql-upload

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ require("dotenv").config();
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { getUser } from "./gql/user/users.utils";
-import {typeDefs, resolvers} from "./schema";
+import { typeDefs, resolvers } from "./schema";
 import { graphql } from "graphql";
+import { graphqlUploadExpress } from 'graphql-upload'
 
 const PORT = process.env.PORT
 
 const apollo = new ApolloServer({
   typeDefs,
   resolvers,
-  context: async ({req}) => {
+  uploads: false,
+  context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token)
     }
@@ -18,9 +20,10 @@ const apollo = new ApolloServer({
 })
 
 const app = express();
-apollo.applyMiddleware({app});
+app.use(graphqlUploadExpress());
+apollo.applyMiddleware({ app });
 app.use("/static", express.static("uploads"));
 
-app.listen({port: PORT}, () => {
-    console.log(`🚀 Server is running on http://localhost:${PORT}/graphql ✅`);
-  });
+app.listen({ port: PORT }, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}/graphql ✅`);
+});
