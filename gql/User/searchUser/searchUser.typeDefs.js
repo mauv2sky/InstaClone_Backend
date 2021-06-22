ae593936f660cdd37a74a84c8344f6fba2fffe20
mauv2sky <mauv2sky@gmail.com>
2021-06-22 10:15:36 +0900

    fix result type of searchUser typeDefs

--- gql/User/searchUser/searchUser.typeDefs.js
+++ gql/User/searchUser/searchUser.typeDefs.js
@@ -1,12 +1,7 @@
 import { gql } from "apollo-server";
 
 export default `
-    type searchUserResult {
-        ok: Boolean!
-        error: String
-        matchUser: [User]
-    }
     type Query {
-        searchUser(keyword: String!): searchUserResult!
+        searchUser(keyword: String!): [User]
     }
 `
