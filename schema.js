a70ee897bb1b2c19ca0369284776267ab717227d
mauv2sky <mauv2sky@gmail.com>
2022-04-27 00:39:05 +0900

    [Modify] 안 쓰는 import 모듈 삭제

--- schema.js
+++ schema.js
@@ -1,4 +1,4 @@
-import { loadFilesSync,mergeResolvers,mergeTypeDefs,makeExecutableSchema } from "graphql-tools";
+import { loadFilesSync, mergeResolvers, mergeTypeDefs } from "graphql-tools";
 
 const loadedTypeDefs = loadFilesSync(`${__dirname}/gql/**/*.typeDefs.js`)
 const loadedResolvers = loadFilesSync(`${__dirname}/gql/**/*.resolvers.js`)
