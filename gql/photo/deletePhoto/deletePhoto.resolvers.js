25133ff0488534330b079b9f6008f30f2792c6f6
mauv2sky <mauv2sky@gmail.com>
2022-04-27 00:38:16 +0900

    [Add] S3 객체 삭제 로직 추가

--- gql/photo/deletePhoto/deletePhoto.resolvers.js
+++ gql/photo/deletePhoto/deletePhoto.resolvers.js
@@ -1,40 +1,44 @@
-import client from "../../../client";
-import { protectedResolver } from "../../user/users.utils";
-import { PrismaDelete } from "@paljs/plugins";
+import client from '../../../client';
+import { deleteFromS3 } from '../../shared/shared.utils';
+import { protectedResolver } from '../../user/users.utils';
+import { PrismaDelete } from '@paljs/plugins';
 
 export default {
     Mutation: {
         deletePhoto: protectedResolver(async (_, { id }, { loggedInUser }) => {
             const photo = await client.photo.findUnique({
                 where: {
-                    id
+                    id,
                 },
                 select: {
-                    userId: true
-                }
+                    userId: true,
+                    file: true,
+                },
             });
             if (!photo) {
                 return {
                     ok: false,
-                    error: "photo not found."
-                }
+                    error: 'photo not found.',
+                };
             } else if (photo.userId !== loggedInUser.id) {
                 return {
                     ok: false,
-                    error: "Not authorized."
-                }
+                    error: 'Not authorized.',
+                };
             } else {
+                const data = await deleteFromS3(photo.file);
+
                 const prismaDelete = new PrismaDelete(client);
                 await prismaDelete.onDelete({
                     model: 'Photo',
                     where: { id },
-                    deleteParent: true
-                })
+                    deleteParent: true,
+                });
 
                 return {
-                    ok: true
-                }
+                    ok: true,
+                };
             }
-        })
-    }
-}
+        }),
+    },
+};
--- gql/shared/shared.utils.js
+++ gql/shared/shared.utils.js
@@ -3,9 +3,10 @@ import AWS from 'aws-sdk';
 AWS.config.update({
     credentials: {
         accessKeyId: process.env.AWS_KEY,
-        secretAccessKey: process.env.AWS_SECRET
-    }
-})
+        secretAccessKey: process.env.AWS_SECRET,
+        region: process.env.AWS_REGION,
+    },
+});
 
 export const uploadToS3 = async (file, userId, folderName) => {
     const { filename, createReadStream } = await file;
@@ -13,10 +14,23 @@ export const uploadToS3 = async (file, userId, folderName) => {
     const objectName = `${folderName}/${userId}-${Date.now()}-${filename}`;
     const { Location } = await new AWS.S3()
         .upload({
-            Bucket: "test-instaclone-uploads",
+            Bucket: 'test-instaclone-uploads',
             Key: objectName,
-            ACL: "public-read",
-            Body: readStream
-        }).promise();
+            ACL: 'public-read',
+            Body: readStream,
+        })
+        .promise();
     return Location;
-}
+};
+
+export const deleteFromS3 = async (url) => {
+    let routes = url.split('/').slice(-2).join('/');
+    const data = new AWS.S3()
+        .deleteObject({
+            Bucket: 'test-instaclone-uploads',
+            Key: routes,
+        })
+        .promise();
+
+    return data;
+};
