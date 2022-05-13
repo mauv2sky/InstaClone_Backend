8baf78ea0fe9c8ae2aca7fff090d041bf8ac09f9
mauv2sky <mauv2sky@gmail.com>
2022-05-13 13:40:55 +0900

    [Add] Photo 댓글수, 댓글가져오기, 좋아요 여부 resolver 추가

--- gql/photo/photos.resolvers.js
+++ gql/photo/photos.resolvers.js
@@ -1,50 +1,79 @@
-import client from "../../client"
+import client from '../../client';
 
 export default {
     Photo: {
         user: ({ userId }) => {
             return client.user.findUnique({
                 where: {
-                    id: userId
-                }
-            })
+                    id: userId,
+                },
+            });
         },
         hashtags: ({ id }) => {
             return client.hashtag.findMany({
                 where: {
                     photos: {
-                        some: { id }
-                    }
-                }
-            })
+                        some: { id },
+                    },
+                },
+            });
         },
         likes: ({ id }) => client.like.count({ where: { photoId: id } }),
-        comments: ({ id }) => client.comment.count({ where: { photoId: id } }),
+        commentNumber: ({ id }) => client.comment.count({ where: { photoId: id } }),
+        comments: ({ id }) =>
+            client.comment.findMany({
+                where: {
+                    photoId: id,
+                },
+                include: {
+                    user: true,
+                },
+            }),
         isMine: ({ userId }, _, { loggedInUser }) => {
             if (!loggedInUser) {
                 return false;
             }
             return userId === loggedInUser.id;
-        }
+        },
+        isLiked: async ({ id }, _, { loggedInUser }) => {
+            if (!loggedInUser) return false;
+
+            const ok = await client.like.findUnique({
+                where: {
+                    photoId_userId: {
+                        photoId: id,
+                        userId: loggedInUser.id,
+                    },
+                },
+                select: {
+                    id: true,
+                },
+            });
+
+            if (!ok) return false;
+            return true;
+        },
     },
     Hashtag: {
         photos: ({ id }, { page }) => {
-            return client.hashtag.findUnique({
-                where: {
-                    id
-                }
-            }).photos();
+            return client.hashtag
+                .findUnique({
+                    where: {
+                        id,
+                    },
+                })
+                .photos();
         },
         totalPhotos: ({ id }) =>
             client.photo.count({
                 where: {
                     hashtags: {
                         some: {
-                            id
-                        }
-                    }
-                }
-            })
+                            id,
+                        },
+                    },
+                },
+            }),
     },
     Comment: {
         isMine: ({ userId }, _, { loggedInUser }) => {
@@ -52,6 +81,6 @@ export default {
                 return false;
             }
             return userId === loggedInUser.id;
-        }
-    }
-}
+        },
+    },
+};
--- gql/photo/photos.typeDefs.js
+++ gql/photo/photos.typeDefs.js
@@ -1,4 +1,4 @@
-import { gql } from "apollo-server";
+import { gql } from 'apollo-server';
 
 export default gql`
     type Photo {
@@ -7,12 +7,14 @@ export default gql`
         file: String!
         caption: String
         likes: Int!
-        comments: Int!
+        commentNumber: Int!
+        comments: [Comment]
         isMine: Boolean!
+        isLiked: Boolean!
         hashtags: [Hashtag]
         createdAt: String!
         updatedAt: String!
-    },
+    }
     type Hashtag {
         id: Int!
         hashtag: String!
@@ -20,14 +22,14 @@ export default gql`
         totalPhotos: Int!
         createdAt: String!
         updatedAt: String!
-    },
+    }
     type Like {
         id: Int!
         user: User!
         photo: Photo!
         createdAt: String!
         updatedAt: String!
-    },
+    }
     type Comment {
         id: Int!
         user: User!
@@ -37,4 +39,4 @@ export default gql`
         createdAt: String!
         updatedAt: String!
     }
-`
+`;
